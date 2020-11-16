05302475ca0abe21e0c75c3c2253d5c0b4e16be0
joshtune <joshua.tune@gmail.com>
2020-11-15 18:25:41 -0700

    auto deployment updates

--- common.js
+++ common.js
@@ -150,7 +150,7 @@ DATA.announcements = [
     },
     {
         title: 'Sat 5 Dec - Primary Baptism',
-        description: 'Primary Baptism, Ward Level, 8:00am, <a href="https://goo.gl/maps/3yFKCXzoFB3hzTZN7" target="_blank">Lehi YSA Stake Building 1550 South 1100 West</a>',
+        description: 'Primary Baptism - <a href="https://goo.gl/maps/3yFKCXzoFB3hzTZN7" target="_blank">Lehi YSA Stake Building 1550 South 1100 West</a>',
     },
     {
         title: 'Sun 6 Dec - Fast Sunday',
